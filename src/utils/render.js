b0e8e11306ee5abce2ab69038c0b7d8df7f9f7af
swlws <979623074@qq.com>
2025-11-21 16:20:41 +0800

    ejs

--- src/utils/render.js
+++ src/utils/render.js
@@ -3,7 +3,6 @@ import path from 'node:path';
 import ejs from 'ejs';
 
 export async function copyTemplate(srcDir, destDir, data = {}) {
-  console.log(`\n ✨ 创建目录：${destDir}`);
   fs.mkdirSync(destDir);
 
   const files = fs.readdirSync(srcDir);
@@ -15,7 +14,6 @@ export async function copyTemplate(srcDir, destDir, data = {}) {
     const stat = fs.statSync(src);
 
     if (stat.isDirectory()) {
-      // fs.mkdirSync(dest);
       await copyTemplate(src, dest, data);
     } else {
       const content = fs.readFileSync(src, 'utf8');
--- templates/node-mjs/src/main.mjs
+++ templates/node-mjs/src/main.mjs
@@ -1,1 +1,3 @@
 console.log('mjs');
+
+const str = `<%= projectName %>`;
